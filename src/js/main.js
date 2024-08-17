e44f50b1970075fe98bcd3434dcb7db7844cac2e
Oleksandr Ratushnyi <oleksandr.ratushnyi@namecheap.com>
2024-08-17 13:02:51 +0300

    connect client content

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log('main');
+import content from '../../index.json';
+
+document.title = content.seo.title;
+
+document.getElementById('home-root').innerHTML = `
+<h1>${content.title}</h1>
+<nav>
+    <a href="/contact/">Contact</a>
+    <a href="/about/">About Us</a>
+</nav>
+`
