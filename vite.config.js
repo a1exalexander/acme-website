841567352c6e235a9fc719e468b520f341e54cfc
Oleksandr Ratushnyi <oleksandr.ratushnyi@namecheap.com>
2024-08-17 10:48:59 +0300

    create website template

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about/index.html"),
+        contact: resolve(__dirname, "contact/index.html"),
+      },
+    },
+  },
+});
