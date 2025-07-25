50430593cfbd7992d1f4b95861c825c5fd0607da
Oleksandr Ratushnyi <oleksandr.ratushnyi@namecheap.com>
2025-07-25 09:56:56 +0300

    Add preview configuration to Vite for local development

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  preview: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: ['all']
+  },
   build: {
     rollupOptions: {
       input: {
